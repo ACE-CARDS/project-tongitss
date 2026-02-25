e6499bbdd395fc67e5bef7a11d0489f4780b1786
Magsuwellan <factora63@gmail.com>
2026-02-25 23:46:34 +0800

    chore(initial commit): created index.php, folder structure, and basic navbar
    - made index.php
    - basic navbar.php
    - folder structure with temporary files
    - website logo
    - included .htaccess for security
    - added tailwind css via cdn

---
+++ components/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center p-4">
+    <p>&copy; 2024 ACE CARDS. All rights reserved.</p>
+</footer>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>ACE CARDS</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+
+        <link rel="stylesheet" href="style.css">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> -->
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+
+    <body>
+        <?php include ("components/includes/navbar.php") ?>
+
+        <main>
+            <div class="hero-section">
+                <h1 class="text-4xl font-bold text-center">ACE CARDS</h1>
+                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nisl nunc eget nisl.</p>
+            </div>
+        
+        </main>
+        
+        <?php include ("components/includes/footer.php") ?>
+    </body>
+</html>
