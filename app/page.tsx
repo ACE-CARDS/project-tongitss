46433daf9f70aa0cccf7f403203fcc225a7def4f
Magsuwellan <factora63@gmail.com>
2026-02-28 16:11:13 +0800

    feat(navbar): responsive navbar
    - Changes to hamburger menu depending on screensize
    - Changes title depending on page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <div className="">
       <NavBar />
       <main className="">
-        <section id="hero" className="justify-center items-center text-center py-20 bg-red-500 h-screen">
-          <h1 className="text-3xl font-bold">Welcome to ACE CARDS!</h1>
-          <p className="text-lg mt-4">This is the home page of our card game application.</p>
+        <section id="hero" className="justify-center items-center text-center py-20 bg-white h-screen">
+          <h1 className="text-3xl font-bold text-black">Welcome to ACE CARDS!</h1>
+          <p className="text-lg mt-4 text-black">This is the home page of our card game application.</p>
         </section>
         
         <section id="wow" className="justify-center items-center text-center py-20 bg-yellow-500 h-screen">
