7f91be7c7eb163b3139b23169ce18a4ab4378d1f
maryonpagodna <mtvillagracia@up.edu.ph>
2026-03-03 17:34:34 +0800

    fix: kidla now visible

--- app/page.tsx
+++ app/page.tsx
@@ -28,14 +28,6 @@ export default function Home() {
         </section>
 
         <Kidla onClick={() => setIsModalShowing(true)} />
-        <section>
-          <button
-            onClick={() => setIsModalShowing(true)}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          >
-            Button
-          </button>
-        </section>
 
         <section
           id="wow"
--- components/kidlaButton.tsx
+++ components/kidlaButton.tsx
@@ -1,9 +1,10 @@
 "use client";
+import Image from "next/image";
+
 export default function Kidla({ onClick }: { onClick: () => void }) {
   return (
-    <button
-      onClick={onClick}
-      className="text-white font-bold py-2 px-4 rounded"
-    ></button>
+    <button onClick={onClick} className="p-0 w-37" id="kidla">
+      <Image src="/images/D2.png" alt="" width={2278} height={3719} />
+    </button>
   );
 }
