24ea5cee6220ed95c3a893761c31112ec739f937
maryonpagodna <mtvillagracia@up.edu.ph>
2026-04-07 01:41:59 +0800

    fix: fixed hardcoding in calendar

--- components/announcements.tsx
+++ components/announcements.tsx
@@ -33,11 +33,10 @@ export default function Dashboard() {
     getAnnouncements();
   }, []);
 
-    console.log(user);
+  console.log(user);
 
   return (
     <div className="text-[#141414] min-h-screen">
-
       <main className="mx-auto w-[95%] lg:w-[90%] max-w-[1400px] lg:py-12">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
           {/* Calendar */}
@@ -75,7 +74,6 @@ export default function Dashboard() {
           </div>
         </div>
       </main>
-
     </div>
   );
 }
