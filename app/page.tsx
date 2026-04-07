9693c6ecfaf32871386ae037fa2dbf192b4d1088
slimbaro <slimbaro@up.edu.ph>
2026-04-07 12:59:10 +0800

    (fix): logo for back to top

--- app/page.tsx
+++ app/page.tsx
@@ -351,11 +351,21 @@ useEffect(() => {
           }}
           className="fixed bottom-6 left-10 z-[10000] bg-white/80 backdrop-blur-md hover:bg-white shadow-xl border border-white/40 px-4 py-3 rounded-full flex items-center gap-2 transition-all duration-300 hover:scale-105"
         >
-          <img
-            src="/assets/logos/up1.png"
-            alt="Back to hero"
-            className="w-5 h-5 object-contain"
+          { /* //https://heroicons.com/outline */}
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth="2" 
+            stroke="currentColor" 
+            className="w-5 h-5 text-[#011638]"
+          >
+          <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" 
           />
+          </svg>
           <span className="text-sm font-semibold text-[#011638] hidden sm:block">
             Back to Top
           </span>
