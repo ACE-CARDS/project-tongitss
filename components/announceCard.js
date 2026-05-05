326f2766cb6b9419d35b49579df14c12406f46fc
maryonpagodna <mtvillagracia@up.edu.ph>
2026-05-05 21:43:31 +0800

    fix: added dates in landing announcements

--- components/announceCard.js
+++ components/announceCard.js
@@ -1,7 +1,25 @@
 const AnnounceCard = ({ announce_landing }) => {
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    return new Date(dateString).toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+  };
+
+  const isSame =
+    announce_landing.announce_landing_start ===
+    announce_landing.announce_landing_end;
+
   return (
     <div className="mb-4 p-4 rounded-md border-1 border-[#eff0f2] pr-6 pl-6">
-      <h3 className="text-xl font-bold text-[#eff0f2] pb-2">
+      <p className="text-[#eff0f2] text-justify text-sm opacity-60">
+        {formatDate(announce_landing.announce_landing_start)}
+        {!isSame && ` - ${formatDate(announce_landing.announce_landing_end)}`}
+      </p>
+
+      <h3 className="text-2xl font-bold text-[#eff0f2] pb-2 hyphens-auto break-words">
         {announce_landing.announce_landing_title}
       </h3>
       <div className="opacity-85">
