3c1ec09b8b293b584e323bacf9ee70e66155a7a9
mpfactora <factora63@gmail.com>
2026-05-28 01:11:42 +0800

    fix(about us mission): added Variants for type checking in ts

--- app/about-us/about-mission.tsx
+++ app/about-us/about-mission.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 export default function AboutMission() {
   const containerVariants = {
@@ -11,7 +11,7 @@ export default function AboutMission() {
     }
   };
 
-  const itemVariants = {
+  const itemVariants: Variants = {
     hidden: { opacity: 0, y: 30 },
     show: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } }
   };
