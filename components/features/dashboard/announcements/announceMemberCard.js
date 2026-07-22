ce3501c5f16af76ef6503007d635820de12d22e3
slimbaro <slimbaro@up.edu.ph>
2026-07-22 17:40:28 +0800

    fix(announceMemberCard): add break-words to strictly fit text into container

--- components/features/dashboard/announcements/announceMemberCard.js
+++ components/features/dashboard/announcements/announceMemberCard.js
@@ -2,11 +2,11 @@ const AnnounceMemberCard = ({ announce_dash }) => {
   
   return (
     <div className="ml-4 mb-4 p-4 rounded-xl border-1 border-l-4 border-l-[#011638] border-[#d7d7d7] pr-6 pl-6 bg-white">
-      <h3 className="text-l font-bold text-[#141414] pb-2 pt-2">
+      <h3 className="text-l font-bold text-[#141414] pb-2 pt-2 break-words">
         {announce_dash.announce_dash_title}
       </h3>
       <div className="opacity-85">
-        <p className="text-[#141414] text-justify text-sm">
+        <p className="text-[#141414] text-justify text-sm break-words">
           {announce_dash.announce_dash_desc}
         </p>
       </div>
